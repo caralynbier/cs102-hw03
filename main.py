aa8c8d53112027327ef94e7d18c388362da40072
caralyn <caralyn711@gmail.com>
2019-11-20 19:37:29 -0500

    finished

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import sys
+from statistics import mean
 
 
 def main():
+
     assert len(sys.argv) > 1, "No input file path specified."
 
     input_file_path = sys.argv[1]
     print(f"Processing input file: {input_file_path}")
 
-    # TODO: Fill in the actual logic here!
+    f = open(input_file_path,"r")
+    content = f.read()
+
+    arr = content.split("\n")
+
+    for i in range(len(arr)):
+        lst = arr[i].split(",")
+        num_list = []
+        for val in lst:
+            if val != '':
+                num_list.append(float(val))
+        print(mean(num_list))
 
 
 if __name__ == "__main__":
